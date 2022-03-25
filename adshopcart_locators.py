99e01702782238c1c45bf41743515d6f150c2287
unknown <svglaz@yahoo.ca>
2022-03-24 23:27:52 -0700

    first commit

---
+++ adshopcart_locators.py
@@ -0,0 +1,3 @@
+app = 'Advantage Shopping Cart'
+adv_shop_cart_url = 'https://advantageonlineshopping.com/#/'
+adv_shop_cart_home_page_title = '\u00A0Advantage Shopping'
---
+++ adshopcart_methods.py
@@ -0,0 +1,45 @@
+import datetime
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import adshopcart_locators as locators
+
+s = Service(executable_path='../chromedriver.exe')
+driver = webdriver.Chrome(service=s)
+
+
+def setUp():
+    print(f'Launch {locators.app} App')
+    print(f'---------------------------- ~*~ ---------------------------------')
+    print(f'The test is started at: {datetime.datetime.now()}')
+
+    # make browser full screen
+    driver.maximize_window()
+
+    # give browser up to 30 seconds to respond
+    driver.implicitly_wait(30)
+
+    # navigate to Moodle App website
+    driver.get(locators.adv_shop_cart_url)
+
+    # check that Moodle URL and the home page title are as expected
+    if driver.current_url == locators.adv_shop_cart_url and driver.title == locators.adv_shop_cart_home_page_title:
+        print(f'We are at Advantage Shopping homepage -- {driver.current_url}')
+        print(f'{locators.app} Homepage title: {driver.title}')
+        sleep(0.25)
+        tearDown()
+    else:
+        print(f'{locators.app} did not launch. Check your code or application!')
+        print(f'Current URL: {driver.current_url}, Page title: {driver.title}')
+        tearDown()
+
+
+def tearDown():
+    if driver is not None:
+        print(f'----------------------- ~*~ ------------------------------')
+        print(f'The test is completed at: {datetime.datetime.now()}')
+        sleep(0.5)
+        driver.close()
+        driver.quit()
+
+setUp()
